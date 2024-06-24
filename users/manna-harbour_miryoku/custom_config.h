fe51c06015b235f640c305dc971f85209b446404
PiousAeneas <53350783+PiousAeneas@users.noreply.github.com>
2024-06-24 10:54:42 -0400

    Update custom_config.h
    Added Excel Shortcuts

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -11,10 +11,11 @@
     2. U_TABB / U_TABF are Win/Mac compatible shortcuts for toggling back / forward through browser tabs.
     3. U_BRWSR_BCK / U_BRWSR_FWD are Win/Mac compatible for navigating backwards / forwards in a browser.
     4. U_SEARCH enables Spotlight search for both Windows and Mac.
+    5. Excel Shortcuts: New Window, Freeze, Indent, Outdent, Add Decimal, Remove Decimal
 */
 
 #define MIRYOKU_LAYER_MEDIA \
-TD(U_TD_BOOT),    TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              RGB_TOG,        U_BRWSR_BCK,      U_TABB,      U_TABF,     U_BRWSR_FWD,       \
-KC_LGUI,          KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_SEARCH,       KC_MPRV,          KC_VOLD,     KC_VOLU,    KC_MNXT,           \
-TD(U_TD_WIN),     KC_ALGR,           TD(U_TD_U_FUN),    TD(U_TD_U_MEDIA),  TD(U_TD_MAC),      OU_AUTO,        U_NU,             U_NU,        U_NU,       U_NU,              \
-U_NP,             U_NP,              U_NA,              U_NA,              U_NA,              KC_MSTP,        KC_MPLY,          KC_MUTE,     U_NP,       U_NP
+TD(U_TD_BOOT),    TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              U_XWIN,         U_BRWSR_BCK,      U_TABB,      U_TABF,      U_BRWSR_FWD,       \
+KC_LGUI,          KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_SEARCH,       KC_MPRV,          KC_VOLD,     KC_VOLU,     KC_MNXT,           \
+TD(U_TD_WIN),     KC_ALGR,           TD(U_TD_U_FUN),    TD(U_TD_U_MEDIA),  TD(U_TD_MAC),      U_XFRZ,         U_XOUT,           U_XDECDEC,   U_XDECINC,   U_XIND,            \
+U_NP,             U_NP,              U_NA,              U_NA,              U_NA,              KC_MSTP,        KC_MPLY,          KC_MUTE,     U_NP,        U_NP
