ad5a8ad3f20018fb57ae81a1971b0cd90c9b1cce
PiousAeneas <53350783+PiousAeneas@users.noreply.github.com>
2024-06-24 11:14:27 -0400

    Update custom_rules.mk
    Changed comment formatting.

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,7 +1,9 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
-// Firmware too large
+/*
+	Firmware too large: https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+*/
 EXTRAFLAGS      += -flto
 COMMAND_ENABLE   = no # Commands for debug and configuration
 UNICODE_ENABLE   = no# Unicode
