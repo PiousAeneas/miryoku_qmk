2d96b13d3fffe64d3d58ddc834c08c2abc2801cb
PiousAeneas <53350783+PiousAeneas@users.noreply.github.com>
2024-06-24 12:10:48 -0400

    Update custom_rules.mk
    Updated Comment

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,7 +1,7 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
-# Firmware too large: https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+# Optimizations to reduce firmware size: https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
 LTO_ENABLE       = yes   # Enables Link Time Optimization
 CONSOLE_ENABLE   = no    # Disables Console for debug
 COMMAND_ENABLE   = no    # Disables Commands for debug and configuration
