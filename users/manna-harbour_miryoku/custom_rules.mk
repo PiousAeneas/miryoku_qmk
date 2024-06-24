52c2ae1e269215c499379a866837d070c4fe0677
PiousAeneas <53350783+PiousAeneas@users.noreply.github.com>
2024-06-24 11:03:52 -0400

    Update custom_rules.mk
    Settings to reduce firmware size.

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,5 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+// Firmware too large: https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+EXTRAFLAGS        += -flto
